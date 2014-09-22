e4e1a7d6ae7f57dc911ca3600877eca4e2de4b65
Sebastian Wong <sebastian.wong@hotmail.com>
2014-09-23 02:15:39 +0800

    Testing speech recognition
    Basic test for speech recognition

---
+++ Speechrecognition.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import speech_recognition as sr
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+	audio = r.listen(source)
+
+
+try:
+	print("You said " + r.recognize(audio))
+
+except LookupError:
+	print("Could not understand audio.")
